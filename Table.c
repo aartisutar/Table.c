74d0cce836ab0b23a86a5860aba9173f48b4811f
Aarti <103598054+aartisutar@users.noreply.github.com>
2022-04-18 09:45:34 +0530

    C program table.c

---
+++ Table.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+
+{
+    printf("\t  \t \t %-50s\n \n ","C programming Sutdent");
+    printf("%20s %20s %20s\n"," Student Name","Subject","Percentage");
+    printf("%20s %19s %17s\n","Aarti","6 sub","80%");
+    printf("%20s %19s %17s\n"," Ashwini","6 sub","81%");
+    printf("%20s %19s %17s\n","Himani","6 sub","82%");
+    printf("%20s %19s %17s\n","Yash","6 sub","83%");
+    printf("%20s %19s %17s\n","Sahil","6 sub","84%");
+    printf("%20s %19s %17s\n","Aniket","6 sub","85%");
+    getch();
+}
+
